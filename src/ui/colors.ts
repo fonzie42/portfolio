a9413910cb3c6c3ff7477831a957eb6f1d4afc4b
Afonso <ferrer.afonso@gmail.com>
2023-07-29 22:34:24 -0300

    [ui] Added colors and its theme variations

---
+++ src/ui/colors.ts
@@ -0,0 +1,29 @@
+export const COLORS = {
+  main: "#F9E9E4",
+  primary: "#733347",
+  primaryAlt: "#421D31",
+  accent: "#F04E45",
+  accent2: "#F35E53",
+  accent3: "#F57164",
+  accent4: "#F78878",
+  accent5: "#F9A090",
+  alt: "#85BD4D",
+  alt2: "#63a53b",
+};
+
+export const COLORS_DARK_MODE = {
+  main: "#1C1D35",
+  primaryAlt: "#1B5183",
+  primary: "#D0F0EF",
+  accent: "#00C7F2",
+  accent2: "#00D8F6",
+  accent3: "#00E4F9",
+  accent4: "#00ECFB",
+  accent5: "#00F2FC",
+  alt: "#7F41CF",
+  alt2: "#EEF6F7",
+};
+
+export const getRGBA = {
+  primaryAlt: (opacity: number) => `rgba(66, 29, 49, ${opacity})`,
+};
