99a76c42c33d24bf42b66a7b0f18f95938e68aab
Afonso <ferrer.afonso@gmail.com>
2023-08-01 20:42:50 -0300

    [fix] Updated global text colors

--- src/theme/globalStyle.ts
+++ src/theme/globalStyle.ts
@@ -13,7 +13,23 @@ export const GlobalStyle = createGlobalStyle`
   button {
     margin: 0;
     padding: 0;
+  }
+  
+  h1,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6{
     color: ${({ theme }) => theme.primary};
+  }
+
+  p,
+  code,
+  button {
+    
+    color: ${({ theme }) => theme.primaryAlt};
   }
 
   body {
