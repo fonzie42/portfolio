fd73cb2751d009fc4695890e450e29c5535e6dd9
Afonso <ferrer.afonso@gmail.com>
2023-08-02 23:42:54 -0300

    [feat] Updated home to use ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import { ThemeProvider } from "styled-components";
-
 import "./App.css";
 import { GlobalStyle } from "./theme/globalStyle";
-import { COLORS, COLORS_DARK_MODE } from "./ui/colors";
-import { useState } from "react";
 
-import { Toggle } from "./ui/Toggle";
+import { ThemeContextProvider } from "./context/themeContextProvider";
 
 function App({ children }: { children: JSX.Element }) {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
-
-  const isDarkThemeSelected = theme === "dark";
-  const onToggleCallback = () =>
-    setTheme(isDarkThemeSelected ? "light" : "dark");
-
   return (
-    <ThemeProvider theme={isDarkThemeSelected ? COLORS_DARK_MODE : COLORS}>
+    <ThemeContextProvider>
       <GlobalStyle />
-      <Toggle
-        checked={isDarkThemeSelected}
-        onToggleCallback={onToggleCallback}
-      />
       {children}
-    </ThemeProvider>
+    </ThemeContextProvider>
   );
 }
 
