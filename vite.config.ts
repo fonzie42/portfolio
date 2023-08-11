b59510349fd0795121a6c01d7a19d7a2e2988d4a
Afonso Ferrer <13241113+fonzie42@users.noreply.github.com>
2023-08-10 21:39:52 -0300

    Revert "[chore] added missing plugin for vite's path resolution"

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react()],
     base: "/",
   };
 
