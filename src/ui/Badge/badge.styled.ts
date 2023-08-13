5a8d2c7ee88dc5ebdc312da9d90b3d8f53859b2a
Afonso Ferrer <aferrer1@whopper.com>
2023-08-13 12:36:08 -0300

    [feat] Added styles and props for Badge component

--- src/ui/Badge/badge.styled.ts
+++ src/ui/Badge/badge.styled.ts
@@ -1,3 +1,10 @@
 import styled from "styled-components";
 
-export const BadgeWrapper = styled.div``;
+export const BadgeWrapper = styled.div`
+  border-radius: 16px;
+  background: ${({ theme }) => theme.accent5};
+  color: ${({ theme }) => theme.primary};
+  display: inline-block;
+  padding: 6px 12px;
+  font-size: 16px;
+`;
--- src/ui/Badge/badge.tsx
+++ src/ui/Badge/badge.tsx
@@ -2,6 +2,6 @@ import { FC } from "react";
 import { BadgeProps } from "./types";
 import { BadgeWrapper } from "./badge.styled";
 
-export const Badge: FC<BadgeProps> = () => {
-  return <BadgeWrapper></BadgeWrapper>;
+export const Badge: FC<BadgeProps> = ({ label }) => {
+  return <BadgeWrapper>{label}</BadgeWrapper>;
 };
