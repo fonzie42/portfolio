1f87361b577a42bcb065bacdb0fa7d1904bfdcb1
Afonso Ferrer <aferrer1@whopper.com>
2023-08-13 12:07:20 -0300

    [chore] Created basis for badge component

---
+++ src/ui/Badge/badge.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+import { BadgeProps } from "./types";
+import { BadgeWrapper } from "./badge.styled";
+
+export const Badge: FC<BadgeProps> = () => {
+  return <BadgeWrapper></BadgeWrapper>;
+};
